2c9af4b0ab74ecd311165dd74c67b69ad882d999
stefania sanchez <stefani2019sanchez@gmail.com>
2024-09-26 15:56:00 -0500

    se hicieron mejoras

--- sketch.js
+++ sketch.js
@@ -1,13 +1,19 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  frameRate(4);
+  frameRate(30);
   background(0);
   rectMode(CENTER);
 }
 function draw() {
-  background(random(100, 151), random(0, 90), 100);
-  fill(mouseY, 100, 67);
+  background(random(100, 151), random(0, 90), 100, 10);
+  //el cuarto valor es transparencia
+  strokeWeight(5);
+  noFill();
+  stroke(mouseY, 255, 0);
   circle(mouseX + 80, mouseY, mouseX);
+
+  strokeWeight(1);
+  stroke(0, 255.0);
   fill(200, 200, 10);
   rect(mouseX, mouseY, 100, 100);
 }
