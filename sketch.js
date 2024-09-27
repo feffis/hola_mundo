6a5accc1284eea0a92bc89cc645926e437fcaf84
stefania sanchez <stefani2019sanchez@gmail.com>
2024-09-27 05:20:07 -0500

    Se realizo ajustes al diseño

--- sketch.js
+++ sketch.js
@@ -1,19 +1,40 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  frameRate(30);
+  frameRate(60);
   background(0);
   rectMode(CENTER);
 }
+
 function draw() {
-  background(random(100, 151), random(0, 90), 100, 10);
-  //el cuarto valor es transparencia
-  strokeWeight(5);
+  //Color del fondo RGB y el cuarto valor es la transparencia
+  background(40, random(0, 95), 102, 10);
+
+  strokeWeight(2);
+  stroke(mouseY, 255, 255);
   noFill();
-  stroke(mouseY, 255, 0);
-  circle(mouseX + 80, mouseY, mouseX);
+  circle(mouseX, mouseY + 40, 60, 60);
 
   strokeWeight(1);
-  stroke(0, 255.0);
-  fill(200, 200, 10);
-  rect(mouseX, mouseY, 100, 100);
+  noStroke();
+  fill(94, 213, 235);
+  circle(mouseX, mouseY + 40, 50, 60);
+
+  strokeWeight(1);
+  stroke(mouseY, 255, 255);
+  fill(94, 213, 235);
+  frameRate(45);
+  let biteSize = PI / 24;
+  let startAngle = biteSize * sin(frameCount * 0.6) + biteSize;
+  let endAngle = TWO_PI - startAngle;
+  arc(mouseX, mouseY, 80, 80, startAngle, endAngle, PIE);
+
+  strokeWeight(1);
+  noStroke();
+  fill(255, 255, 255);
+  circle(mouseX + 120, mouseY - 10, 50, 30);
+
+  strokeWeight(1);
+  noStroke();
+  fill(255, 255, 255);
+  circle(mouseX, mouseY + 40, 30, 10);
 }
